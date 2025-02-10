f78a594361d369480175cab1833525e1dc6c7ae2
caparniyazi <capar.niyazi.tr@gmail.com>
2025-02-10 12:48:42 +0300

    Angular training.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SalesPersonListComponent} from './sales-person-list/sales-person-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SalesPersonListComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
